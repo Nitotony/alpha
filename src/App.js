f4e933cf5d1b442a6feb6dd498526759ee8d2034
Nito Tony C <nitotony47@gmail.com>
2024-08-05 15:34:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Anna/>
+//       <De/>
+//       <Arma/>
+//     </div>
+    
+//   );
+// }
+
+
+// const Arma=()=>{
+//   return(
+//     <div>
+//       <h1>Armas</h1>
+//     </div>
+//   );
+// }
+
+
+// const Anna=()=>{
+//   return(
+//     <div>
+//       <h1>Anna</h1>
+//     </div>
+//   );
+
+
+// }
+
+// const De=()=>{
+//   return(
+//     <div>
+//       <h1>De</h1>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+class MyWebsite extends React.Component{
+
+render(){
+  return <h1>Lol</h1>
 }
 
-export default App;
+}
+
+
+
+
+
+ReactDOM.render(<MyWebsite/>,document.getElementById('root'));
+
+
+export default MyWebsite;
